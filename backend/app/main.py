e2790c0cdf7249b8089c190a0e71c99880828b7a
Danyil Suhak <danyil.suhak@cdev.com.ua>
2024-03-28 21:48:15 +0200

    added posts views

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,8 +5,11 @@ from sqlalchemy.orm import Session
 from . import models, database
 from passlib.hash import bcrypt
 import jwt
+from jwt.exceptions import DecodeError
 
 app = FastAPI()
+
+SECRET_KEY = "secret_key"
 
 class UserCreate(BaseModel):
     email: str
@@ -34,6 +37,15 @@ def create_access_token(data: dict, secret_key: str, expires_delta: int):
     encoded_jwt = jwt.encode(to_encode, secret_key, algorithm="HS256")
     return encoded_jwt
 
+def get_email_from_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        email = payload.get("email")
+        return email
+    except DecodeError:
+        print("her")
+        return None
+
 @app.post("/signup")
 def create_user_endpoint(user: UserCreate, db: Session = Depends(database.get_db)):
     existing_user = get_user_by_email(db, user.email)
@@ -44,7 +56,7 @@ def create_user_endpoint(user: UserCreate, db: Session = Depends(database.get_db
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
-    token = create_access_token(data={"email": user.email}, secret_key="secret_key", expires_delta=3600) # Expires in 1 hour
+    token = create_access_token(data={"email": user.email}, secret_key=SECRET_KEY, expires_delta=3600) # Expires in 1 hour
     return {"token": token}
 
 @app.post("/login")
@@ -52,7 +64,7 @@ def login(user: UserLogin, db: Session = Depends(database.get_db)):
     db_user = get_user_by_email(db, user.email)
     if not db_user or not verify_password(user.password, db_user.password):
         raise HTTPException(status_code=401, detail="Incorrect email or password")
-    token = create_access_token(data={"email": user.email}, secret_key="secret_key", expires_delta=3600) # Expires in 1 hour
+    token = create_access_token(data={"email": user.email}, secret_key=SECRET_KEY, expires_delta=3600) # Expires in 1 hour
     return {"token": token}
 
 # Post views
@@ -79,32 +91,45 @@ def get_posts(db: Session):
 def get_posts_by_user(db: Session, user_id: int):
     return db.query(models.Post).filter(models.Post.author_id == user_id).all()
 
-def delete_post(db: Session, post_id: int):
-    db_post = db.query(models.Post).filter(models.Post.id == post_id).first()
+def delete_post(db: Session, post_id: int, user_id: int):
+    db_post = db.query(models.Post).filter(models.Post.id == post_id, models.Post.author_id == user_id).first()
     if not db_post:
-        return None
+        raise HTTPException(status_code=404, detail="Post not found or does not belong to the user")
     db.delete(db_post)
     db.commit()
     return db_post
 
 @app.post("/add-post")
-def create_post_endpoint(post: Post, db: Session = Depends(database.get_db)):
-    # user = get_user_by_email(db, token)
-    return create_post(db, post)
+def create_post_endpoint(post: Post, token: str = Depends(TokenAuth), db: Session = Depends(database.get_db)):
+    email = get_email_from_token(token.token)
+    user = get_user_by_email(db, email)
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    return create_post(db, text=post.text, author_id=user.id)
 
 @app.get("/get-posts")
-def get_posts_endpoint(db: Session = Depends(database.get_db)):
-    return get_posts(db)
+def get_posts_endpoint(token: str = Depends(TokenAuth), db: Session = Depends(database.get_db)):
+    email = get_email_from_token(token.token)
+    user = get_user_by_email(db, email)
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    return get_posts_by_user(db, user.id)
 
 @app.delete("/delete-post")
-def delete_post_endpoint(post: PostDelete, db: Session = Depends(database.get_db)):
-    return delete_post(db, post.post_id)
+def delete_post_endpoint(post: PostDelete, token: str = Depends(TokenAuth), db: Session = Depends(database.get_db)):
+    email = get_email_from_token(token.token)
+    user = get_user_by_email(db, email)
+    if not user:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    return delete_post(db, post.post_id, user.id)
 
 @app.get("/db_init")
 def db_init():
     database.Base.metadata.create_all(database.engine)
     return "good"
-
 
 
 @app.get("/")
